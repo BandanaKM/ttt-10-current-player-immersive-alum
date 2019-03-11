8f061ca14b83c557f12a083d90f134051329efee
Bana Malik <bandanakm@gmail.com>
2019-03-11 02:55:37 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,7 +3,7 @@ def turn_count(board)
   counter = 0
   board.each do | b |
      b.strip!
-     if b == "O" || b == "X" || b == " O "
+     if b == "O" || b == "X"
        counter += 1
      end
   end
